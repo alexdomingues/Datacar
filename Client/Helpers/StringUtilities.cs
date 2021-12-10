7bc3c41a72798c614470a2800228f13569c9926a
Alexandre Domingues <alexandre.domingues@live.com>
2021-12-10 12:23:41 +0000

    Add project files.

---
+++ Client/Helpers/StringUtilities.cs
@@ -0,0 +1,8 @@
+﻿namespace Datacar.Client.Helpers
+{
+    public class StringUtilities
+    {
+        // Transform string to upper case
+        public static string CustomToUpper(string value) => value.ToUpper();
+    }
+}
---
+++ Shared/Entities/Cars.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datacar.Shared.Entities
+{
+    public class Cars
+    {
+            public Guid Id { get; set; }
+            [Required]
+            public string Matricula { get; set; }
+            //[Required]
+            //public DateTime? DataMaricula { get; set; }
+            //[Required]
+            //public string Modelo { get; set; }
+            //[Required]
+            //public string TipoVeiculo { get; set; }
+            //[Required]
+            //public int KmsIniciais { get; set; }
+            //[Required]
+            //public int KmsContratados { get; set; }
+            //[Required]
+            //public DateTime? ValidadeContrato { get; set; }
+            //[Required]
+            //public int IdCondutor { get; set; }
+            //[Required]
+            //public string IdentificadorViaVerde { get; set; }
+            //[Required]
+            //public string Url { get; set; }
+    }
+}
