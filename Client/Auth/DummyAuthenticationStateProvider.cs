090fa16f3cd2efc79ecf04a275be468a2144f6fe
alexdomingues <31892337+alexdomingues@users.noreply.github.com>
2022-01-05 00:13:14 +0000

    Revert "."

--- Client/Auth/DummyAuthenticationStateProvider.cs
+++ Client/Auth/DummyAuthenticationStateProvider.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -10,13 +8,15 @@ namespace Datacar.Client.Auth
     public class DummyAuthenticationStateProvider : AuthenticationStateProvider
     {
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
-        {   
+        {
             var anonymous = new ClaimsIdentity(new List<Claim>() {
-                new Claim("key1", "value1"),
-                new Claim(ClaimTypes.Name, "Alexandre"),
-                new Claim(ClaimTypes.Role,"Admin")
-            },"test");
+            new Claim("key2","Value1"),
+            new Claim(ClaimTypes.Name,"Alexandre"),
+            new Claim(ClaimTypes.Role,"Admin")
+            });
+
             return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
+
         }
     }
 }
